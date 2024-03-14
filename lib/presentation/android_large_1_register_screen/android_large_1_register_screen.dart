84ed28af2ce6e62a60659d10fd2439d813562fa2
pranitdas8 <pranitdas8@gmail.com>
2024-03-14 17:01:19 +0530

    register UI

--- lib/presentation/android_large_1_register_screen/android_large_1_register_screen.dart
+++ lib/presentation/android_large_1_register_screen/android_large_1_register_screen.dart
@@ -7,6 +7,10 @@ import 'package:attendance_app/core/app_export.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:attendance_app/presentation/widgets/auth_backgound.dart';
+import 'package:attendance_app/presentation/widgets/auth_textfields.dart';
+import 'package:attendance_app/presentation/widgets/auth_texts.dart';
+import 'package:attendance_app/presentation/widgets/buttons.dart';
 
 class AndroidLarge1RegisterScreen extends StatefulWidget {
   AndroidLarge1RegisterScreen({Key? key}) : super(key: key);
@@ -28,253 +32,63 @@ class _AndroidLarge1RegisterScreenState
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SizedBox(
-          width: double.maxFinite,
-          child: Column(
-            children: [
-              _buildSix(context),
-              SizedBox(height: 45.v),
-              SizedBox(
-                height: 512.v,
-                width: double.maxFinite,
-                child: Stack(
-                  alignment: Alignment.topLeft,
-                  children: [
-                    CustomImageView(
-                      imagePath: ImageConstant.imgDesignUpperBlue200,
-                      height: 216.v,
-                      width: 201.h,
-                      alignment: Alignment.bottomRight,
-                    ),
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Container(
-                        margin: EdgeInsets.only(right: 107.h),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 10.h,
-                          vertical: 16.v,
-                        ),
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: fs.Svg(
-                              ImageConstant.imgGroup3,
-                            ),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: 29.v,
-                                  width: 28.h,
-                                  child: CustomImageView(
-                                    imagePath:
-                                        ImageConstant.imgGroup1BlueGray100,
-                                    margin: EdgeInsets.only(bottom: 3.v),
-                                  ),
-                                ),
-                                SizedBox(
-                                    width: 10
-                                        .h), // Add spacing between image and text
-                                SizedBox(
-                                  height: 40.v,
-                                  width: 150.h,
-                                  child: TextField(
-                                    decoration: const InputDecoration(
-                                      hintText: "Name",
-                                      border: OutlineInputBorder(),
-                                    ),
-                                    controller: nameController,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 24.v),
-                            Row(
-                              children: [
-                                SizedBox(
-                                  height: 24.adaptSize,
-                                  width: 24.adaptSize,
-                                  child: CustomImageView(
-                                    imagePath: ImageConstant.imgMageEmail,
-                                    margin:
-                                        EdgeInsets.only(top: 6.v, bottom: 2.v),
-                                  ),
-                                ),
-                                SizedBox(
-                                    width: 10
-                                        .h), // Add spacing between icon and text field
-                                Expanded(
-                                  child: TextField(
-                                    decoration: const InputDecoration(
-                                      hintText: "Email",
-                                      border: OutlineInputBorder(),
-                                    ),
-                                    controller: emailController,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 30.v),
-                            Padding(
-                              padding: EdgeInsets.only(left: 3.h, right: 47.h),
-                              child: Row(
-                                children: [
-                                  SizedBox(
-                                    height: 25.v,
-                                    width: 23.h,
-                                    child: CustomImageView(
-                                      imagePath: ImageConstant.imgGroup11,
-                                      margin: EdgeInsets.only(
-                                          top: 6.v, bottom: 1.v),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                      width: 13
-                                          .h), // Add spacing between icon and text field
-                                  Expanded(
-                                    child: TextField(
-                                      decoration: const InputDecoration(
-                                        hintText: "Employee ID",
-                                        border: OutlineInputBorder(),
-                                      ),
-                                      controller: employeeIdController,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 34.v),
-                            Padding(
-                              padding: EdgeInsets.only(right: 50.h),
-                              child: Row(
-                                children: [
-                                  SizedBox(
-                                    height: 24.adaptSize,
-                                    width: 24.adaptSize,
-                                    child: CustomImageView(
-                                      imagePath: ImageConstant.imgGroup12,
-                                      margin: EdgeInsets.only(
-                                          top: 5.v, bottom: 4.v),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                      width: 16
-                                          .h), // Add spacing between icon and text field
-                                  Expanded(
-                                    child: TextField(
-                                      decoration: const InputDecoration(
-                                        hintText: "Department",
-                                        border: OutlineInputBorder(),
-                                      ),
-                                      controller: departmentController,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 28.v),
-                            Padding(
-                              padding: EdgeInsets.only(left: 4.h, right: 18.h),
-                              child: Row(
-                                children: [
-                                  CustomImageView(
-                                    imagePath: ImageConstant.imgGroup13,
-                                    height: 24.adaptSize,
-                                    width: 24.adaptSize,
-                                    margin: EdgeInsets.symmetric(vertical: 4.v),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(left: 12.h),
-                                    child: TextButton(
-                                      onPressed: _getImage,
-                                      child: const Text(
-                                        "Capture",
-                                        style: TextStyle(
-                                          color: Colors
-                                              .blue, // Change the text color to match the button style
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            ElevatedButton(
-                              onPressed: _storeDataInFirestore,
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.blue,
-                                foregroundColor: Colors.white,
-                                elevation: 3,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                              ),
-                              child: Container(
-                                height: 50.h,
-                                width: double.infinity,
-                                decoration: const BoxDecoration(),
-                                child: const Center(
-                                  child: Text(
-                                    "Submit",
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w700),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+    final AuthTexts authTexts = AuthTexts();
+    var _formKey = GlobalKey<FormState>();
 
-  /// Section Widget
-  Widget _buildSix(BuildContext context) {
-    return SizedBox(
-      height: 216.v,
-      width: double.maxFinite,
-      child: Stack(
-        alignment: Alignment.bottomCenter,
+    return AuthBackgroundScaffold(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          CustomImageView(
-            imagePath: ImageConstant.imgDesignUpper,
-            height: 178.v,
-            width: 207.h,
-            alignment: Alignment.topLeft,
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Text(
-              "Register",
-              style: theme.textTheme.displaySmall,
+          SizedBox(height: 70),
+          authTexts.title("Register"), // Changed title to "Register"
+          authTexts
+              .subtitle("Please fill in the details below"), // Changed subtitle
+          SizedBox(height: 45),
+          Form(
+            child: Column(
+              children: [
+                AuthTextField(
+                  isPassword: false,
+                  hint: "Name",
+                  controller: nameController,
+                ),
+                SizedBox(height: 10),
+                AuthTextField(
+                  isPassword: false,
+                  hint: "Email",
+                  controller: emailController,
+                ),
+                SizedBox(height: 10),
+                AuthTextField(
+                  isPassword: false,
+                  hint: "Employee ID",
+                  controller: employeeIdController,
+                ),
+                SizedBox(height: 10),
+                AuthTextField(
+                  isPassword: false,
+                  hint: "Department",
+                  controller: departmentController,
+                ),
+              ],
             ),
           ),
-          CustomElevatedButton(
-            width: 150.h,
-            text: "Login",
-            margin: EdgeInsets.only(top: 63.v),
-            buttonStyle: CustomButtonStyles.outlinePrimaryLR30,
-            alignment: Alignment.topRight,
-            onPressed: _storeDataInFirestore,
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomElevatedButton(
+                width: 150,
+                text: "Capture",
+                onPressed: _getImage,
+              ),
+              SizedBox(width: 20),
+              CustomElevatedButton(
+                width: 150,
+                text: "Submit",
+                onPressed: _storeDataInFirestore,
+              ),
+            ],
           ),
         ],
       ),
@@ -285,7 +99,7 @@ class _AndroidLarge1RegisterScreenState
     final ImagePicker _picker = ImagePicker();
     final XFile? image = await _picker.pickImage(
         source: ImageSource
-            .camera); // Change source to ImageSource.camera for capturing from camera
+            .camera); // Changed source to ImageSource.camera for capturing from camera
     setState(() {
       _imageFile = image != null ? File(image.path) : null;
     });
